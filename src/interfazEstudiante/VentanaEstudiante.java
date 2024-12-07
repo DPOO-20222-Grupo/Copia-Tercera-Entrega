371e3a1d09a2e8690eb71bf578a5ce9a2bbe6ec4
CesxrAugusto <c.espinosag@uniandes.edu.co>
2024-12-07 00:22:33 -0500

    pipipi

--- src/interfazEstudiante/VentanaEstudiante.java
+++ src/interfazEstudiante/VentanaEstudiante.java
@@ -6,32 +6,26 @@ import javax.swing.JPanel;
 import interfaz.Aplicacion;
 import user.Estudiante;
 
+@SuppressWarnings("serial")
 public class VentanaEstudiante extends JFrame{
 	
-	private Aplicacion app;
-	
+	private Aplicacion aplicacion;
 	private Estudiante estudiante;
-	
 	private PanelOpcionesEstudiante panelOpciones; 
-	
 	private JPanel panelContenido;
 	
-	public VentanaEstudiante(Aplicacion app, Estudiante estudiante) {
-		this.app = app;
-		this.estudiante = estudiante;
+	public VentanaEstudiante(Aplicacion app, Estudiante est) {
+		this.aplicacion = app;
+		this.estudiante = est;
 		
 		this.panelOpciones = new PanelOpcionesEstudiante(estudiante);
-		
 		this.add(panelOpciones);
 		
 		this.setVisible(true);
-		
 		this.setSize(600, 400);
-
-		
-		
-		
-		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null); 
+        this.setResizable(false);
 	}
 
 }
